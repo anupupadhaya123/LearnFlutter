7ec5d48eb90b246dc48aafd61480f92d0670a834
Kaaji <au.anupupadhaya@gmail.com>
2022-03-13 21:19:59 +0545

    themes extracted

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -6,7 +6,21 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Catalog App"),
+        title: Text("Catalog App", style: TextStyle(
+          color: Colors.black
+        ),),
+
+        
+        // centerTitle: true,
+        // backgroundColor: Colors.white,
+        // elevation: 0,
+        // iconTheme: IconThemeData(color: Colors.black),
+        // title: Text("Catalog App" ,style: TextStyle(
+        //   color: Colors.black,
+          
+          
+        // ),),
+        
       ),
       body: Center(
         child: Container(
@@ -17,3 +31,4 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+ 
