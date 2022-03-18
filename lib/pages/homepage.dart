e7cd2f9e5cf5ce3237a74a2dd8c3d6055a3ec4b3
Kaaji <au.anupupadhaya@gmail.com>
2022-03-18 22:34:07 +0545

    day12 Completed "Json Maping , load decode Json"

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,20 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:learn_flutter/models/catalog.dart';
 import 'package:learn_flutter/widgets/drawer.dart';
 import 'package:learn_flutter/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  //loading Json
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    // print(catalogJson);
+    // Decoding json
+    var decodeData = jsonDecode(catalogJson);
+    // print(decodeData);
+    var productsData = decodeData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+    // print(productsData);
+  }
+
   @override
   Widget build(BuildContext context) {
-
-    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
+    // final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
 
     return Scaffold(
       appBar: AppBar(
-        title: Text("Catalog App", style: TextStyle(
-          color: Colors.black
-        ),),
-
+        title: Text(
+          "Catalog App",
+          style: TextStyle(color: Colors.black),
+        ),
 
         // centerTitle: true,
         // backgroundColor: Colors.white,
@@ -22,28 +51,24 @@ class HomePage extends StatelessWidget {
         // iconTheme: IconThemeData(color: Colors.black),
         // title: Text("Catalog App" ,style: TextStyle(
         //   color: Colors.black,
-          
-          
+
         // ),),
-        
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-
+        child: (CatalogModel.items!=null && CatalogModel.items.isNotEmpty)? ListView.builder(
           // itemCount: CatalogModel.items.length,
-          itemCount: dummyList.length,
+          itemCount: CatalogModel.items.length,
           itemBuilder: (context, index) {
             return ItemWidget(
               // item: CatalogModel.items[index] ,
 
-              item: dummyList[index] ,
+              item: CatalogModel.items[index],
             );
-          } ,
-          ),
+          },
+        ):Center(child: CircularProgressIndicator(),)
       ),
       drawer: MyDrawer(),
     );
   }
 }
- 
